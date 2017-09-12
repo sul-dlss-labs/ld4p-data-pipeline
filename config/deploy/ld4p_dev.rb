11e547644d1a0b7750808769c2925a69e18febf9
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-09-12 16:25:25 -0700

    Capistrano default_env with LD4P_DATA and BOOTSTRAP_SERVERS

--- config/deploy/ld4p_dev.rb
+++ config/deploy/ld4p_dev.rb
@@ -1,69 +1,20 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
 
 # spark masters
-server "ld4p_dev_spark_master", user: "root", roles: %w{redhat spark master}
+server 'ld4p_dev_spark_master', user: 'root', roles: %w{redhat spark master}
 
-# spark slaves
-server "ld4p_dev_spark_slave1", user: "root", roles: %w{redhat spark slave}
-server "ld4p_dev_spark_slave2", user: "root", roles: %w{redhat spark slave}
-server "ld4p_dev_spark_slave3", user: "root", roles: %w{redhat spark slave}
+# spark workers
+server 'ld4p_dev_spark_worker1', user: 'root', roles: %w{redhat spark worker}
+server 'ld4p_dev_spark_worker2', user: 'root', roles: %w{redhat spark worker}
+server 'ld4p_dev_spark_worker3', user: 'root', roles: %w{redhat spark worker}
 
+# ----
+# Setup the environment variables for the spark app
 
-# role-based syntax
-# ==================
+set :ld4p_data, File.join(fetch(:deploy_to), 'current', 'src', 'main', 'resources', 'xsl')
+set :bootstrap_servers, 'ec2-34-213-81-65.us-west-2.compute.amazonaws.com:9092,ec2-34-214-42-7.us-west-2.compute.amazonaws.com:9092,ec2-52-36-184-167.us-west-2.compute.amazonaws.com:9092'
 
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+set :default_env, {
+  LD4P_DATA: fetch(:ld4p_data),
+  BOOTSTRAP_SERVERS: fetch(:bootstrap_servers)
+}
 
