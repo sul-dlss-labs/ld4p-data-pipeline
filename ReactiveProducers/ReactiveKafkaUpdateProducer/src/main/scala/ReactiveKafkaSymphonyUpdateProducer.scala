e225193011244f138a09139d8da7967b274f9322
Maatari Daniel Okouya <maatariokouya@gmail.com>
2017-09-18 15:21:22 -0700

    Updated the prodcuer to now send the changes to kafka in the marc21Update topic

--- ReactiveProducers/ReactiveKafkaUpdateProducer/src/main/scala/ReactiveKafkaSymphonyUpdateProducer.scala
+++ ReactiveProducers/ReactiveKafkaUpdateProducer/src/main/scala/ReactiveKafkaSymphonyUpdateProducer.scala
@@ -73,12 +73,12 @@ object ReactiveKafkaSymphonyUpdateProducer extends App {
     lines.map {logline =>
       logline.split('^').filter{e => e.startsWith("IQ") || e.startsWith("NQ")}(0)
     }.groupedWithin(500, 1 second).flatMapConcat { datacode => Source(datacode.distinct) }
-      .mapAsync(1)( e =>
+      .mapAsync(4)( e =>
         Future{
           val res = (s"/s/SUL/Bin/LD4P/catDumpUpdate.sh ${e}".!!)
           ByteString(res)
         }
-      )/*.runForeach(s =>println(s"Result: ${s.toString()}"))*/
+      )
     .via(marcFlow).async.via(recordFlow).via(Producer.flow(producerSettings)).map { result =>
         val record = result.message.record
         println(s"Posted message ${record.value} to kafka ${record.topic} topic")
